052b1a8f33cccacb039d5327a4fa20dadd21e004
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-12-04 18:47:58 +0500

    Add the responsive nav

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 import './style.css';
+import './img/PAKtZ.png';
 
-const name = document.querySelector('h2');
+// nav bar
+const menu = document.querySelector('.header-nav');
+const menuItems = document.querySelectorAll('.menuItem');
+const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.fa-circle-xmark');
+const menuIcon = document.querySelector('.menu-Icon');
 
-name.innerHTML = 'Hello, Mujeeb!';
+function toggleMenu() {
+  if (menu.classList.contains('showMenu')) {
+    menu.classList.remove('showMenu');
+    closeIcon.style.display = 'none';
+    menuIcon.style.display = 'block';
+  } else {
+    menu.classList.add('showMenu');
+    closeIcon.style.display = 'block';
+    menuIcon.style.display = 'none';
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu);
+
+menuItems.forEach(
+  (menuItem) => {
+    menuItem.addEventListener('click', toggleMenu);
+  },
+);
+// nav bar end
